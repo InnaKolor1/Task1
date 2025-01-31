6ea557804d0aa41422e38d5052d7d94f1cbcdd50
InnaKolor1 <ineza.permiakova@yandex.ru>
2025-02-01 01:54:56 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-
+var dog=8.0;
+dog=dog+4-3.5;
+System.out.println(" dog = "+dog);
+var cat=3.6;
+cat=cat+4-1.6;
+System.out.println(" cat = "+cat);
+var paper=763789;
+paper=paper+4-7639;
+System.out.println(" paper = "+paper);
+var friend=19;
+friend=(friend+2)/7;
+System.out.println( " friend = "+friend);
+var frog=3.5;
+frog=(frog*10/3.5)+4;
+System.out.println( " frog = "+frog);
+var weightOfTheOneBoxer=78.2;
+System.out.println( " Масса одного боксера = "+weightOfTheOneBoxer);
+var weightOfTheTwoBoxer=82.7;
+System.out.println( " Масса второго боксера = "+weightOfTheTwoBoxer);
+var totalWeight=weightOfTheOneBoxer+weightOfTheTwoBoxer;
+System.out.println( " Общая масса двух бойцов = "+totalWeight);
+var remains=weightOfTheOneBoxer%weightOfTheTwoBoxer;
+System.out.println( " Остаток = "+remains);
+var watch=640;
+var human=(watch/8)+94;
+System.out.println( " Всего работников в компании- "+human+" человек ");
+var totalHours=watch/human;
+System.out.println(" Если в компании работает "+ human+" человек, то всего "+totalHours+" часов работы может быть поделено между сотрудниками ");
     }
 }
